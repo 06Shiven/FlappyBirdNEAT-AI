839c264d59218115922e8b100c02f8ccef4090d4
Shiven Jaiswal <shivenjaiswal02@gmail.com>
2025-05-12 16:09:57 -0400

    Create bird.py

---
+++ bird.py
@@ -0,0 +1,56 @@
+import pygame
+import os
+
+BIRD_IMG = pygame.image.load(os.path.join("imgs", "bird1.png"))
+SCALE = pygame.transform.scale(BIRD_IMG, (40, 30))
+IMGS = [SCALE]
+
+MAX_ROTATION = 25
+ROT_VEL = 20
+ANIMATION_TIME = 5
+
+class Bird:
+    IMGS = IMGS
+    MAX_ROTATION = 25
+    ROT_VEL = 20
+    ANIMATION_TIME = 5
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.tilt = 0  # rotation
+        self.tick_count = 0
+        self.vel = 0
+        self.height = self.y
+        self.img = self.IMGS[0]
+
+    def jump(self):
+        self.vel = -10.5
+        self.tick_count = 0
+        self.height = self.y
+
+    def move(self):
+        self.tick_count += 1
+        d = self.vel * self.tick_count + 1.5 * self.tick_count ** 2
+
+        if d >= 16:
+            d = 16
+        if d < 0:
+            d -= 2
+
+        self.y += d
+
+        if d < 0 or self.y < self.height + 50:
+            if self.tilt < self.MAX_ROTATION:
+                self.tilt = self.MAX_ROTATION
+        else:
+            if self.tilt > -90:
+                self.tilt -= self.ROT_VEL
+
+    def draw(self, win):
+        rotated_image = pygame.transform.rotate(self.img, self.tilt)
+        new_rect = rotated_image.get_rect(center=self.img.get_rect(topleft=(self.x, self.y)).center)
+        win.blit(rotated_image, new_rect.topleft)
+
+    def get_mask(self):
+        return pygame.mask.from_surface(self.img)
